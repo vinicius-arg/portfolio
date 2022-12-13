a9378d3ab8f7646af33ec4bd77d03d9010c0ae0c
joão vinícius <j.vin7cius@gmail.com>
2022-12-13 13:18:53 -0300

    feat: animações no container de skills

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,25 +1,34 @@
 const skillsContainer = document.querySelector(".skills__container");
-const skills = [...skillsContainer.children];
-
+const skillsElements = document.querySelectorAll(".skills__container article");
 const back = document.querySelector(".fa-arrow-left");
+
+const skills = [...skillsElements];
+const animationTime = 100;
 
 skills.forEach(element => {
     element.addEventListener("click", event => {
         let id = event.target.id;
         highlightSkill(id);
 
+        executeTransitions();
+
         event.target.classList.remove("--hover-ef");
+        event.target.style.pointerEvents = "none";
 
         back.style.animationName = "fade-in";
+        back.style.pointerEvents = "auto";
     });
 });
 
-back.addEventListener("click", event => {
+back.addEventListener("click", () => {
     back.style.animationName = "fade-out";
+    back.style.pointerEvents = "none";
+
+    executeTransitions();
 
     let gridTemplate = `"a a b""c d d""e e f"`;
-    skillsContainer.style.gridTemplateAreas = gridTemplate;
-})
+    gridTemplateAssign(gridTemplate);
+});
 
 function highlightSkill (s) {
     let otherSkillsId = [];
@@ -32,5 +41,34 @@ function highlightSkill (s) {
     let otherSkillsStr = otherSkillsId.join(" ");
     let gridTemplate = `"${s} ${s} ${s} ${s} ${s}""${s} ${s} ${s} ${s} ${s}""${s} ${s} ${s} ${s} ${s}""${s} ${s} ${s} ${s} ${s}""${otherSkillsStr}"`;
 
-    skillsContainer.style.gridTemplateAreas = gridTemplate;
+    gridTemplateAssign(gridTemplate);
+}
+
+function resetHoverEf (element) {
+    element.classList.add("--hover-ef");
+}
+
+function resetPointerEv (element) {
+    element.style.pointerEvents = "auto";
+}
+
+function animateGrid () {
+    skills.forEach(element => {
+        element.style.animationName = "fade-out";
+        setTimeout(() => {
+            element.style.animationName = "";
+        },200); // to reset animation
+    });
+}
+
+function gridTemplateAssign (template) {
+    setTimeout(() => {
+        skillsContainer.style.gridTemplateAreas = template;
+    }, animationTime);
+}
+
+function executeTransitions () {
+    animateGrid();
+    skills.forEach(resetHoverEf);
+    skills.forEach(resetPointerEv);
 }
