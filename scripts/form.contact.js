c0678478bbf1342c99648c6ddb5ec6bf40fea139
joão vinícius <j.vin7cius@gmail.com>
2022-12-17 16:49:08 -0300

    feat: estrutura do formulário de contato

---
+++ scripts/form.contact.js
@@ -0,0 +1,6 @@
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const msg = document.querySelector("#message");
+const submit = document.querySelector("#submit");
+
+console.log(name, email, msg, submit);
