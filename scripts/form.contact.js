a49d631f4d0cce8620afd10a97f4febcbc79a830
joão vinícius <j.vin7cius@gmail.com>
2022-12-17 20:02:38 -0300

    feat: javascript aplicado no formulário (validação de campos)

--- scripts/form.contact.js
+++ scripts/form.contact.js
@@ -1,6 +1,48 @@
 const name = document.querySelector("#name");
 const email = document.querySelector("#email");
-const msg = document.querySelector("#message");
+const message = document.querySelector("#message");
 const submit = document.querySelector("#submit");
 
-console.log(name, email, msg, submit);
+const inputArray = [name, email, message];
+
+let ableSubmit = {
+    name: false,
+    email: false,
+    message: false
+};
+
+inputArray.forEach(element => {
+    element.addEventListener("input", function () {
+        let id = element.id;
+        if (this.value)  {
+            ableSubmit[id] = true;
+        } else {
+            ableSubmit[id] = false;
+        }
+
+        classAssign();
+    });
+});
+
+function classAssign () {
+    if (verifySubmit()) {
+        submit.classList.add("submit--enabled");
+    } else {
+        submit.classList.remove("submit--enabled");
+    }
+}
+
+function verifySubmit () {
+    const requiredKeys = 3;
+    let count = 0;
+
+    for (key in ableSubmit) {
+        if (ableSubmit[key]) count++;
+    }
+
+    if (count === requiredKeys) {
+        return true;
+    } else {
+        return false;
+    }
+}
