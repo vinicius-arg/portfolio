df1e267925b9c3d73be41e9e8a292d6e13e8e15b
joão vinícius <j.vin7cius@gmail.com>
2022-12-20 11:28:20 -0300

    style: indentação de objetos em 'scrollreveal.effect.js'

--- scripts/scrollreveal.effect.js
+++ scripts/scrollreveal.effect.js
@@ -10,17 +10,32 @@ ScrollReveal({
     } // to reset inputed scroll reveals 
 }); // default configs
 
-ScrollReveal().reveal("section", { delay: 300, distance: 0 } );
+ScrollReveal().reveal("section", { 
+    delay: 300, 
+    distance: 0 
+});
 
-ScrollReveal().reveal(".presents__container .__photo", { delay: 300, distance: "200px" } );
+ScrollReveal().reveal(".presents__container .__photo", { 
+    delay: 300,
+    distance: "200px",
+    mobile: false 
+});
+
 ScrollReveal().reveal(".presents__container article"); 
 
-ScrollReveal().reveal(".carousel", { delay: 300 } );
-ScrollReveal().reveal(stCardImg);
-ScrollReveal().reveal(stCardDescription, { delay: 900 });
+ScrollReveal().reveal(".carousel", { 
+    delay: 300 
+});
 
+ScrollReveal().reveal(stCardImg);
+ScrollReveal().reveal(stCardDescription, { 
+    delay: 900 
+});
 
 ScrollReveal().reveal(".skills__container");
 
-ScrollReveal().reveal(".form-container", { delay: 300 });
+ScrollReveal().reveal(".form-container", { 
+    delay: 300 
+});
+
 ScrollReveal().reveal(".social-media");
